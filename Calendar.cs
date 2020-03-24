38120e44c5ad68f4b389185a5067a7856f85898d
Nicholas Brown <trickynicky0327@hotmail.com>
2020-03-23 20:45:16 -0400

    changed stuff

---
+++ Calendar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolPlanner
+{
+    /// <summary>
+    /// This will manage the calendar
+    /// </summary>
+    class Calendar
+    {
+    }
+}
---
+++ Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolPlanner
+{
+    class Event
+    {
+        public bool complete;
+        public string name;
+        public string description;
+    }
+}
